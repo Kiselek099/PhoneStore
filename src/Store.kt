b37d914b7d5b7a65766f4c78724ba8d497be8b5b
Kiselev <kiselek777099@gmail.com>
2024-12-05 19:50:06 +0300

    Создание класса Store и функции продажи телефона

---
+++ src/Store.kt
@@ -0,0 +1,13 @@
+abstract class Store(val city: String) {
+    val phones = mutableMapOf<String, Int>()
+    val sales = mutableMapOf<String, Int>()
+    open fun buyPhone(model: String) {
+        if (phones.containsKey(model)) {
+            val price = phones[model]!!
+            sales[model] = sales.getOrDefault(model, 0) + 1
+            println("Вы купили $model за $price руб.")
+        } else {
+            println("Такой модели нет в наличии.")
+        }
+    }
+}
